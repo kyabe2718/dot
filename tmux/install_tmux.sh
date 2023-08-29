c9e70c85ce39d105db87894492505adc514090fa
kyabe2718 <kyabetsu.3141@gmail.com>
2023-08-30 01:05:57 +0900

    install_tmux

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -7,27 +7,33 @@ if [ -z ${DOTFILES_HOME:+} ]; then
 fi
 source $DOTFILES_HOME/lib/utils.sh
 
+function install_tmux() {
+    git clone https://github.com/tmux/tmux.git -b 3.3a $HOME/.cache/tmux
+    cd $HOME/.cache/tmux
+    sh autogen.sh
+    ./configure --prefix $HOME/.local && make install
+}
+
 function check_tmux(){
     if [ ! type tmux > /dev/null 2>&1 ]; then
         echo "tmux is not installed."
         return 1
     else
-        return 0
-        # tmux_version=$(tmux -V | sed 's/tmux \([0-9]\.[0-9][a-z]*\)/\1/')
-        # minimum_required="3.3"
-        # if verlt $tmux_version $minimum_required; then
-        #     echo "Current tmux version($tmux_version) is not satisfy minimum required($minimum_required)"
-        #     return 1
-        # else
-        #     return 0
-        # fi
+        tmux_version=$(tmux -V | sed 's/tmux \([0-9]\.[0-9][a-z]*\)/\1/')
+        minimum_required="3.3"
+        if verlt $tmux_version $minimum_required; then
+            echo "Current tmux version($tmux_version) is not satisfy minimum required($minimum_required)"
+            return 1
+        else
+            return 0
+        fi
     fi
 }
 
-function install_tmux(){
+function main(){
     if !(check_tmux); then
         case "$(get_platform)" in
-            "Ubuntu") sudo apt install tmux ;;
+            "Ubuntu") install_tmux ;;
             *) echo "failed to install tmux: Unknown platform $(get_platform)" ;;
         esac
     fi
@@ -37,5 +43,5 @@ function install_tmux(){
     ln -sfv $DOTFILES_HOME/tmux/config $HOME/.tmux/
 }
 
-install_tmux
+main
 
