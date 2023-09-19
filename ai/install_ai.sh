bbd0cf96b4b9bf038a26b87a9871eb1b7b894e4c
kyabe2718 <kyabetsu.3141@gmail.com>
2023-09-19 13:56:29 +0900

    update

--- ai/install_ai.sh
+++ ai/install_ai.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0)); pwd -P)
 
-if !(type ai>/dev/null 2>&1); then
-    ln -snf ${SCRIPT_DIR}/run.sh $HOME/.local/bin/ai
+if !(type gpt>/dev/null 2>&1); then
+    ln -snf ${SCRIPT_DIR}/run.sh $HOME/.local/bin/gpt
 fi
 
--- ai/run.sh
+++ ai/run.sh
@@ -3,22 +3,21 @@
 AI_IMAGE_NAME=ai:latest
 SCRIPT_DIR=$(cd $(dirname $(readlink -f $0)); pwd -P)
 
-usage() {
-    echo "Usage: $0 [options]"
-}
-
 build() {
     docker build -t ${AI_IMAGE_NAME} ${SCRIPT_DIR}
-    # docker builder prune -f # clean up
+}
+
+clean() {
+    docker rmi ${AI_IMAGE_NAME}
 }
 
 run() {
     docker run \
-        --mount type=bind,source=$PWD,target=/workspace \
+        $MOUNT \
         --env OPENAI_API_KEY=${OPENAI_API_KEY} \
-        --user $(id -u):$(id -g) \
+        --user $USER \
         --group-add sudo \
-        -it --rm ${AI_IMAGE_NAME} "$@"
+        -it --rm ${AI_IMAGE_NAME} $@
 }
 
 main() {
@@ -26,22 +25,47 @@ main() {
         build
     fi
 
+    cmd="chatgpt-cli"; args=""
+    MOUNT="--mount type=bind,source=$PWD,target=/workspace"
+    USER="user"
     while [[ $# -gt 0 ]]; do
         case "$1" in
-            -h|--help)
-                usage; exit 0 ;;
-            -b|--build)
-                build; exit 0 ;;
-            --clean)
-                docker rmi ${AI_IMAGE_NAME}; exit 0 ;;
-            -c|--command)
-                shift; run "$@"; exit 0 ;;
-            --) shift; break ;;
-            *)
-                echo "Unknown option: $1"; usage; exit 1 ;;
+            -h|--help)  usage; exit 0 ;;
+            -b|--build) build; exit 0 ;;
+            --clean)    clean; exit 0 ;;
+            --cmd)      shift; cmd="$1";;
+            --root)     USER="root";;
+            -c|--cli)          cmd="chatgpt-cli"; MOUNT="" ;;
+            -i|--interpreter)  cmd="interpreter";;
+            -y|--yes)          args="$args --yes" ;;
+            --no-mount)        MOUNT="" ;;
+            --model)    shift; args="$args --model $1";;
+            --gpt-3.5)         args="$args --model gpt-3.5-turbo";;
+            --gpt-4)           args="$args --model gpt-4";;
+            --)         shift; args="$args $@"; break ;;
+            *) echo "Unknown option: $1"; usage; exit 1 ;;
         esac
+        shift
     done
-    run "interpreter" "-y" "$@"
+    # echo "$cmd$args"
+    run "$cmd$args"
+}
+
+usage() {
+    cat <<EOF
+Usage: $(basename $0) [OPTIONS] [--] [ARGS...]
+  -h, --help            Show this help message and exit.
+  -c, --cli             Run chatgpt-cli.
+  -i, --interpreter     Run interpreter.
+  --no-mount            Do not mount current directory.
+  --model MODEL         Model to use.
+  --gpt-3.5             Use gpt-3.5-turbo model.
+  --gpt-4               Use gpt-4 model.
+  --root                Run as root.
+  --cmd CMD             Command to run in docker container.
+  -b, --build           Build docker image.
+  --clean               Clean docker image.
+EOF
 }
 
 main "$@"
